ec80ca4c48a1679e0191931f8bc19afd13867d08
Amzkit <amzkit@gmail.com>
2021-03-25 16:04:14 +0700

    Set default value for oauth path

--- config/oauth.php
+++ config/oauth.php
@@ -2,9 +2,9 @@
 
 return [
 
-    'authorize_url'     =>  env('OAUTH_SERVER').env('OAUTH_AUTHORIZE_PATH'),
-    'request_token_url' =>  env('OAUTH_SERVER').env('OAUTH_REQUEST_TOKEN_PATH'),
-    'request_user_url'  =>  env('OAUTH_SERVER').env('OAUTH_REQUEST_USER_PATH'),
+    'authorize_url'     =>  env('OAUTH_SERVER').env('OAUTH_AUTHORIZE_PATH','/oauth/authorize'),
+    'request_token_url' =>  env('OAUTH_SERVER').env('OAUTH_REQUEST_TOKEN_PATH','/oauth/token'),
+    'request_user_url'  =>  env('OAUTH_SERVER').env('OAUTH_REQUEST_USER_PATH','/api/user'),
 
     'callback_redirect_url'      =>  env('CALLBACK_REDIRECT_URI'),
     'client_id'         =>  env('CLIENT_ID'),
